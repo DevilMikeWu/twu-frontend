636d6697b6208ecbeb20cdab49cabb1ced2f6faa
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-13 10:39:53 +0000

    Deploy DevilMikeWu/twu-frontend to github.com/DevilMikeWu/twu-frontend.git:gh-pages

---
+++ precache-manifest.4222fb582abee10804afb875f8bf75c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9127530871bf8edce9c1",
+    "url": "/twu-frontend/static/js/runtime~main.4b19027a.js"
+  },
+  {
+    "revision": "45bb6744175f2cd83c57",
+    "url": "/twu-frontend/static/js/main.bd96931d.chunk.js"
+  },
+  {
+    "revision": "f35c6e5625b2659d7647",
+    "url": "/twu-frontend/static/js/2.873a736e.chunk.js"
+  },
+  {
+    "revision": "45bb6744175f2cd83c57",
+    "url": "/twu-frontend/static/css/main.f69246ab.chunk.css"
+  },
+  {
+    "revision": "f35c6e5625b2659d7647",
+    "url": "/twu-frontend/static/css/2.8f23e7d5.chunk.css"
+  },
+  {
+    "revision": "84ac76fe6235fa94e4b474773c8fbd08",
+    "url": "/twu-frontend/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/twu-frontend/precache-manifest.580c3932bb8e7476b016e3f4bad9d0e7.js"
+  "/twu-frontend/precache-manifest.4222fb582abee10804afb875f8bf75c5.js"
 );
 
 workbox.clientsClaim();
