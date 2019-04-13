da5cf30e0dd28b8c9683e7469e16309199c451d6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-13 08:14:15 +0000

    Deploy DevilMikeWu/twu-frontend to github.com/DevilMikeWu/twu-frontend.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4c5d8403ecb827b485d9078f9554c4b2.js"
+  "/twu-frontend/precache-manifest.75bf66a4191b1b68384215024b3f6c48.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/twu-frontend/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
