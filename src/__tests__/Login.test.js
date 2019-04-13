d92022ca6b828e6604b2940b873ee00700bb4d3e
Justin Wang <junting.wang@thoughtworks.com>
2019-04-13 22:14:01 +0800

    add unit test and content for login page

--- src/__tests__/Login.test.js
+++ src/__tests__/Login.test.js
@@ -1,11 +1,121 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./../App";
+import { checkAccountValidation, checkPasswordValidation } from "./../utils/login";
 
-describe("Render Login Page", () => {
-  it("renders without crashing", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
-  });
+test('Account justin should be true', () => {
+  expect(checkAccountValidation('justin')).toBe(true);
+});
+
+test('Account justin666 should be true', () => {
+  expect(checkAccountValidation('justin666')).toBe(true);
+});
+
+test('Account JUSTIN666 should be true', () => {
+  expect(checkAccountValidation('JUSTIN666')).toBe(true);
+});
+
+test('Account Ju6sTi6n should be true', () => {
+  expect(checkAccountValidation('Ju6sTi6n')).toBe(true);
+});
+
+test('Account justin 666 should be false', () => {
+  expect(checkAccountValidation('justin 666')).toBe(false);
+});
+
+test('Account justin_66 should be false', () => {
+  expect(checkAccountValidation('justin_66')).toBe(false);
+});
+
+test('Account justin$%^* should be false', () => {
+  expect(checkAccountValidation('justin$%^*')).toBe(false);
+});
+
+test('Account just$in&666* should be false', () => {
+  expect(checkAccountValidation('just$in&666*')).toBe(false);
+});
+
+test('Account 123321123321 should be false', () => {
+  expect(checkAccountValidation('123321123321')).toBe(false);
+});
+
+test('Account 1justin23 should be false', () => {
+  expect(checkAccountValidation('1justin23')).toBe(false);
+});
+
+test('Account @#$%^&* should be false', () => {
+  expect(checkAccountValidation('@#$%^&*')).toBe(false);
+});
+
+test('Account j should be false', () => {
+  expect(checkAccountValidation('j')).toBe(false);
+});
+
+test('Account justin123123123123123123 should be false', () => {
+  expect(checkAccountValidation('justin123123123123123123')).toBe(false);
+});
+
+test('Account ["justin"] should be false', () => {
+  expect(checkAccountValidation(["justin"])).toBe(false);
+});
+
+
+
+test('Account abc$d1*234 should be true', () => {
+  expect(checkPasswordValidation('abc$d1*234')).toBe(true);
+});
+
+test('Account aN1@z0wCg2D should be true', () => {
+  expect(checkPasswordValidation('aN1@z0wCg2D')).toBe(true);
+});
+
+test('Account ABC1@A7BC2D should be true', () => {
+  expect(checkPasswordValidation('ABC1@A7BC2D')).toBe(true);
+});
+
+test('Account abcd1234&*@# should be true', () => {
+  expect(checkPasswordValidation('abcd1234&*@#')).toBe(true);
+});
+
+test('Account ABCD1234&*@# should be true', () => {
+  expect(checkPasswordValidation('ABCD1234&*@#')).toBe(true);
+});
+
+test('Account a2b3*c@d14&*ke@# should be true', () => {
+  expect(checkPasswordValidation('a2b3*c@d14&*ke@#')).toBe(true);
+});
+
+test('Account abcdabcd, should be false', () => {
+  expect(checkPasswordValidation('abcdabcd,')).toBe(false);
+});
+
+test('Account abCdABcd, should be false', () => {
+  expect(checkPasswordValidation('abCdABcd,')).toBe(false);
+});
+
+test('Account 12345678, should be false', () => {
+  expect(checkPasswordValidation('12345678,')).toBe(false);
+});
+
+test('Account !@#$%^&*, should be false', () => {
+  expect(checkPasswordValidation('!@#$%^&*,')).toBe(false);
+});
+
+test('Account a2b3*9, should be false', () => {
+  expect(checkPasswordValidation('a2b3*9,')).toBe(false);
+});
+
+test('Account a2B3*9 should be false', () => {
+  expect(checkPasswordValidation('a2B3*9')).toBe(false);
+});
+
+test('Account a2b3as1ok23 *9, should be false', () => {
+  expect(checkPasswordValidation('a2b3as1ok23 *9')).toBe(false);
+});
+
+test('Account a2b3as1ok238ui123j^&*1HLJF2323*9, should be false', () => {
+  expect(checkPasswordValidation('a2b3as1ok238ui123j^&*1HLJF2323*9')).toBe(false);
+});
+
+test('Account {name: "aN1@z0wCg2D"}, should be false', () => {
+  expect(checkPasswordValidation({name: "aN1@z0wCg2D"})).toBe(false);
 });
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,10 +1,82 @@
 import React, { Component } from 'react';
+import { Form, Button } from 'react-bootstrap';
+
+import { checkAccountValidation, checkPasswordValidation } from '../../utils/login'
+import './index.css';
 
 class App extends Component {
+  state = {
+    accountInvalid: false,
+    passwordInvalid: false,
+    validated: false
+  }
+
+  handleSubmit = (event) => {
+    const { account, password } = this.form
+    if (!checkAccountValidation(account.value) || !checkPasswordValidation(password.value)) {
+      event.preventDefault()
+      this.setState({
+        accountInvalid: true,
+        passwordInvalid: true,
+      })
+    } else {
+      this.setState({ validated: true });
+    }
+  }
+
   render() {
+    const { accountInvalid, passwordInvalid, validated } = this.state
+
     return (
-      <div className="App">
-        <p>login</p>
+      <div className="login-container">
+        <div className="login-title">
+          <h1>思沃学院在线学习</h1>
+        </div>
+        <div className="form-wrapper">
+          <Form
+            ref={form => this.form = form}
+            validated={validated}
+            onSubmit={e => this.handleSubmit(e)}
+          >
+            <Form.Group controlId="formBasicAccount">
+              <Form.Label>Account</Form.Label>
+              <Form.Control
+                type="text"
+                name="account"
+                isInvalid={accountInvalid}
+                placeholder="Enter Account"
+                required
+              />
+              <Form.Control.Feedback type="invalid">
+                Invalid username or password
+              </Form.Control.Feedback>
+              <Form.Text className="text-muted">
+                账号为2-20位英文,数字
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                name="password"
+                isInvalid={passwordInvalid}
+                placeholder="Enter Password"
+                required
+              />
+              <Form.Control.Feedback type="invalid">
+                Invalid username or password
+              </Form.Control.Feedback>
+              <Form.Text className="text-muted">
+                账号为8-20位英文,数字,标点
+              </Form.Text>
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Login
+            </Button>
+          </Form>
+        </div>
       </div>
     );
   }
