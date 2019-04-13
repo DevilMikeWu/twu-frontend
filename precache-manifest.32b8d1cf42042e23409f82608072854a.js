ad78080f7369795fc58647b0cd5204f6dd474566
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-13 08:01:09 +0000

    Deploy DevilMikeWu/twu-frontend to github.com/DevilMikeWu/twu-frontend.git:gh-pages

---
+++ precache-manifest.32b8d1cf42042e23409f82608072854a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6603110ad63320aeedf1",
+    "url": "/static/js/main.403f5fcf.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "6603110ad63320aeedf1",
+    "url": "/static/css/main.f69246ab.chunk.css"
+  },
+  {
+    "revision": "0e9c487ca48cdc59aae462982fa3daf3",
+    "url": "/index.html"
+  }
+];
