858826d954f0c3fd0efd46b3dafcbce5b0e2a0f5
LiCheng Wu <licheng.wu@CNlicheng.wu>
2019-04-14 13:46:44 +0800

    change browserRouter to hashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 
 import Login from './pages/login'
 import List from './pages/academyList'
