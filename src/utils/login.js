e8eada55bc73a5b63d28a654b4103b2d6983712e
Justin Wang <junting.wang@thoughtworks.com>
2019-04-14 14:09:56 +0800

    fix validation logic

--- src/utils/login.js
+++ src/utils/login.js
@@ -1,21 +1,15 @@
 export const checkAccountValidation = (account) => {
-  if (typeof account !== 'string') return false
-
-  var reg = /^[a-zA-Z][a-zA-Z0-9]{2,20}$/;
-  if (reg.test(account)) {
-    return true
-  } else {
-    return false
-  }
+  var reg = /^[a-zA-Z0-9]{2,20}$/;
+  return checkValidation(account, reg)
 }
 
 export const checkPasswordValidation = (password) => {
-  if (typeof password !== 'string') return false
+  var reg = /^[\w!@#$%^&*,.?]{8,20}$/;
+  return checkValidation(password, reg)
+}
 
-  var reg = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*,.?])[\w!@#$%^&*,.?]{8,20}$/;
-  if (reg.test(password)) {
-    return true
-  } else {
-    return false
-  }
+export const checkValidation = (content, regExp) => {
+  if (typeof content !== 'string') return false
+
+  return regExp.test(content)
 }
